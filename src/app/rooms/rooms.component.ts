35eff3542babe5c1a326de50b9120ede7a9ac22b
Rahmattullah <rfkrhmttllh@gmail.com>
2023-03-30 13:54:39 +0700

    learn Pipe

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -21,34 +21,37 @@ export class RoomsComponent {
 
   roomList: RoomList[] = [
     {
-      roomNumbers : 1,
+      roomNumbers: 1,
       roomType: 'Deluxe Room',
       amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 500,
+      price: 1000000,
       photos:
         'https://images.unplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
       checkinTime: new Date('11-Nov-2021'),
       checkoutTime: new Date('12-Nov-2021'),
+      rating: 4.5,
     },
     {
-      roomNumbers : 2,
+      roomNumbers: 2,
       roomType: 'Deluxe Room',
       amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 1000,
+      price: 1000000,
       photos:
         'https://images.unplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
       checkinTime: new Date('11-Nov-2021'),
       checkoutTime: new Date('12-Nov-2021'),
+      rating: 4.3,
     },
     {
-      roomNumbers : 3,
+      roomNumbers: 3,
       roomType: 'Private Suite Room',
       amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 15000,
+      price: 3000000,
       photos:
         'https://images.unplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
       checkinTime: new Date('11-Nov-2021'),
       checkoutTime: new Date('12-Nov-2021'),
+      rating: 4.9,
     },
   ];
 
