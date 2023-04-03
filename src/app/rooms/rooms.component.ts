1efdb489425f6af7d6f343ec5a4862cd41df0315
Rahmattullah <rfkrhmttllh@gmail.com>
2023-04-03 10:27:28 +0700

    learn lifecycle hook AfterViewInit

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,21 +1,26 @@
-import { Component, DoCheck } from '@angular/core';
+import {
+  Component,
+  DoCheck,
+  ViewChild,
+  OnInit,
+  AfterViewInit,
+} from '@angular/core';
 import { Room, RoomList } from './rooms';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css'],
 })
-export class RoomsComponent implements DoCheck {
+export class RoomsComponent implements OnInit, DoCheck, AfterViewInit {
   hotelName = 'Accor Hotel';
+
+  numberOfRooms = 10;
 
   hideRooms = false;
 
   selectedRoom!: RoomList;
-
-  title = 'Room List';
-
-  numberOfRooms = 10;
 
   rooms: Room = {
     totalRooms: 20,
@@ -23,44 +28,59 @@ export class RoomsComponent implements DoCheck {
     bookedRooms: 5,
   };
 
-  roomList: RoomList[] = [
-    {
-      roomNumbers: 1,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 1000000,
-      photos:
-        'https://images.unplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 4.5,
-    },
-    {
-      roomNumbers: 2,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 1000000,
-      photos:
-        'https://images.unplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 4.3,
-    },
-    {
-      roomNumbers: 3,
-      roomType: 'Private Suite Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 3000000,
-      photos:
-        'https://images.unplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 4.9,
-    },
-  ];
+  title = 'Room List';
+
+  roomList: RoomList[] = [];
+
+  @ViewChild(HeaderComponent, {static: true})
+  headerComponent!: HeaderComponent;
+
+  ngOnInit(): void {
+    console.log(this.headerComponent);
+
+    this.roomList = [
+      {
+        roomNumbers: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+        price: 1000000,
+        photos:
+          'https://images.unplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 4.5,
+      },
+      {
+        roomNumbers: 2,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+        price: 1000000,
+        photos:
+          'https://images.unplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 4.3,
+      },
+      {
+        roomNumbers: 3,
+        roomType: 'Private Suite Room',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+        price: 3000000,
+        photos:
+          'https://images.unplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 4.9,
+      },
+    ];
+  }
 
   ngDoCheck(): void {
     console.log('on changes is called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.headerComponent);
   }
 
   toggle() {
