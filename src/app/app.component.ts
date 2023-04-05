b285aedbc5443438b35ff83ee0064481316d651d
Rahmattullah <rfkrhmttllh@gmail.com>
2023-04-05 11:31:53 +0700

    learn localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, Optional, OnInit } from '@angular/core';
+import { Component, Optional, OnInit, Inject } from '@angular/core';
 import { RoomsComponent } from './rooms/rooms.component';
 import { LoggerService } from './logger.service';
+import { localStorageToken } from './localstorage.token';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,15 @@ import { LoggerService } from './logger.service';
 export class AppComponent implements OnInit {
   title = 'hotelinventoryapp';
 
-  constructor(@Optional() private loggerService: LoggerService) {}
+  constructor(
+    @Optional() private loggerService: LoggerService,
+    @Inject(localStorageToken) private localStorage: any
+  ) {}
 
   ngOnInit() {
     this.loggerService?.log('AppComponent.ngOnInit()');
+
+    this.localStorage.setItem('name', 'Accor Hotel');
   }
 
   // Dynamic Load Component
