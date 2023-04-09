50f18c64c51421cf2b44ad9e79525525ce244f13
Rahmattullah <rfkrhmttllh@gmail.com>
2023-04-09 14:14:07 +0700

    learn ActivatedRoute

--- src/app/rooms/rooms-booking/rooms-booking.component.ts
+++ src/app/rooms/rooms-booking/rooms-booking.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rooms-booking',
   templateUrl: './rooms-booking.component.html',
-  styleUrls: ['./rooms-booking.component.css']
+  styleUrls: ['./rooms-booking.component.css'],
 })
-export class RoomsBookingComponent {
+export class RoomsBookingComponent implements OnInit {
+  id: number = 0;
 
+  id$ = this.router.paramMap.pipe(map((params) => params.get('id')));
+
+  constructor(private router: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // this.id = this.router.snapshot.params['id'];
+    // this.router.params.subscribe((params) => {
+    //   this.id = params['id'];
+    // });
+  }
 }
