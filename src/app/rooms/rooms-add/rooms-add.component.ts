841e78afa5a367c6838b5a2f6b2ae0b3d9a30b84
Rahmattullah <rfkrhmttllh@gmail.com>
2023-04-10 11:04:11 +0700

    learn Template Driven Forms

---
+++ src/app/rooms/rooms-add/rooms-add.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { RoomList } from '../rooms';
+import { RoomsService } from '../services/rooms.service';
+
+@Component({
+  selector: 'app-rooms-add',
+  templateUrl: './rooms-add.component.html',
+  styleUrls: ['./rooms-add.component.css'],
+})
+export class RoomsAddComponent {
+  room: RoomList = {
+    roomType: '',
+    amenities: '',
+    checkinTime: new Date(),
+    checkoutTime: new Date(),
+    photos: '',
+    price: 0,
+    rating: 0,
+    roomNumber: '',
+  };
+
+  successMessage: string = '';
+
+  constructor(private roomsService: RoomsService) {}
+
+  AddRoom() {
+    this.roomsService
+      .addRoom(this.room)
+      .subscribe((data) => (this.successMessage = 'Room Added Successfully'));
+  }
+}
