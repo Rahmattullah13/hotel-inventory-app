c75858c2eab8345a3eee53e1fa68135f83337d7a
Rahmattullah <rfkrhmttllh@gmail.com>
2023-04-11 12:41:20 +0700

    create directive

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,10 +1,35 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  OnInit,
+  HostListener,
+  Input,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appHover]'
+  selector: '[appHover]',
 })
-export class HoverDirective {
+export class HoverDirective implements OnInit {
+  @Input() appHover: string = '#f4f4';
 
-  constructor() { }
+  constructor(private element: ElementRef, private renderer: Renderer2) {
+    console.log(this.element.nativeElement);
+  }
 
+  ngOnInit(): void {
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'backgroundColor',
+      this.appHover
+    );
+  }
+
+  @HostListener('mouseleave') onMouseEnter() {
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'backgroundColor',
+      'white'
+    );
+  }
 }
