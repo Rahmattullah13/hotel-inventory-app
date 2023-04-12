90d8d26e11ffb96d9a573e20c5b1b5a33b765d09
Rahmattullah <rfkrhmttllh@gmail.com>
2023-04-12 10:58:40 +0700

    learn multiple children route

--- src/app/rooms/rooms-routing.module.ts
+++ src/app/rooms/rooms-routing.module.ts
@@ -8,9 +8,11 @@ const routes: Routes = [
   {
     path: 'rooms',
     component: RoomsComponent,
-    children: [{ path: ':id', component: RoomsBookingComponent }],
+    children: [
+      { path: 'add', component: RoomsAddComponent },
+      { path: ':id', component: RoomsBookingComponent },
+    ],
   },
-  { path: 'rooms/add', component: RoomsAddComponent },
 ];
 
 @NgModule({
