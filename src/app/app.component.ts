d69c3f6db4acc559783fdea28baeefdcbe3544be
Rahmattullah <rfkrhmttllh@gmail.com>
2023-04-13 11:32:53 +0700

    learn Router Guard canActivate and canLoad for login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { LoggerService } from './logger.service';
 import { localStorageToken } from './localstorage.token';
 import { InitService } from './init.service';
 import { ConfigService } from './services/config.service';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,12 +18,29 @@ export class AppComponent implements OnInit {
     @Optional() private loggerService: LoggerService,
     @Inject(localStorageToken) private localStorage: any,
     private initService: InitService,
-    private configService: ConfigService
+    private configService: ConfigService,
+    private router: Router
   ) {
     console.log(initService.config);
   }
 
   ngOnInit() {
+    // this.router.events.subscribe((event) => {
+    //   console.log(event);
+    // });
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe((event) => {
+        console.log('Navigation Started!!!');
+      });
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        console.log('Navigation Ended!!!');
+      });
+
     this.loggerService?.log('AppComponent.ngOnInit()');
 
     this.localStorage.setItem('name', 'Accor Hotel');
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  constructor(private route: Router) {}
-
   email: string = '';
   password: string = '';
 
+  constructor(private route: Router, private loginService: LoginService) {}
+
   login() {
-    if (this.email === 'admin@gmail.com' && this.password === '123') {
-      this.route.navigate(['/rooms', 'add']);
+    if (this.loginService.login(this.email, this.password)) {
+      this.route.navigate(['/rooms']);
     }
   }
 }
