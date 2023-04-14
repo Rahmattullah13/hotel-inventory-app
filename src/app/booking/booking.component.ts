3cc9132204dadf90e132f304daedbbd43db25bbc
Rahmattullah <rfkrhmttllh@gmail.com>
2023-04-14 14:30:22 +0700

    learn Ractive Forms

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '../services/config.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.css'],
 })
-export class BookingComponent {
-  constructor(private configService: ConfigService) {}
+export class BookingComponent implements OnInit {
+  bookingForm!: FormGroup;
+
+  constructor(private configService: ConfigService, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.bookingForm = this.fb.group({
+      roomId: [''],
+      guestEmail: [''],
+      checkinDate: [''],
+      checkoutDate: [''],
+      bookingStatus: [''],
+      bookingAmount: [''],
+      bookingDate: [''],
+      mobileNumber: [''],
+      guestName: [''],
+      guestAddress: [''],
+      guestCity: [''],
+      guestState: [''],
+      guestCountry: [''],
+      guestZipCode: [''],
+      guestCount: [''],
+    });
+  }
 }
--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingComponent } from './booking.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    BookingComponent
-  ],
+  declarations: [BookingComponent],
   imports: [
     CommonModule,
-    BookingRoutingModule
-  ]
+    BookingRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
 })
-export class BookingModule { }
+export class BookingModule {}
